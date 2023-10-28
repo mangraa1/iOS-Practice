bb3aa51cf9ac43a6a942a0b599e66dd78e22c7c9
mangraa1 <georgiu.spare@gmail.com>
2023-10-28 14:06:32 +0300

    Unit tests

---
+++ MVP/MVPTests/TestMainModule/MainPresenterTest.swift
@@ -0,0 +1,53 @@
+//
+//  MainPresenterTest.swift
+//  MVPTests
+//
+//  Created by mac on 28.10.2023.
+//
+
+import XCTest
+@testable import MVP
+
+
+class MockView: MainViewProtocol {
+    var titleTest: String?
+
+    func setGreeting(greeting: String) {
+        self.titleTest = greeting
+    }
+}
+
+final class MainPresenterTest: XCTestCase {
+
+    var view: MockView!
+    var person: Person!
+    var presenter: MainPresenter!
+
+    override func setUpWithError() throws {
+        view = MockView()
+        person = Person(firstName: "Baz", secondName: "Bar")
+        presenter = MainPresenter(view: view, person: person)
+    }
+
+    override func tearDownWithError() throws {
+        view = nil
+        person = nil
+        presenter = nil
+    }
+
+    func testModuleIsNotNil() {
+        XCTAssertNotNil(view, "view is not nil")
+        XCTAssertNotNil(person, "person is not nil")
+        XCTAssertNotNil(presenter, "presenter is not nil")
+    }
+
+    func testView() {
+        presenter.showGreeting()
+        XCTAssertEqual(view.titleTest, "Baz Bar")
+    }
+
+    func testPersonModel(){
+        XCTAssertEqual(person.firstName, "Baz")
+        XCTAssertEqual(person.secondName, "Bar")
+    }
+}
